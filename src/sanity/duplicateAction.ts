c0fc9ea86eab205615fe9db5bda7d1bc9a5ed2b9
olamaan <75326761+olamaan@users.noreply.github.com>
2025-09-19 07:58:41 -0400

    stuff

--- src/sanity/duplicateAction.ts
+++ src/sanity/duplicateAction.ts
@@ -1,14 +1,15 @@
 // /src/sanity/duplicateAction.ts
-import { DocumentActionComponent, DocumentActionProps } from 'sanity'
+import { DocumentActionComponent, DocumentActionProps, useClient } from 'sanity'
 
 export const duplicateAction: DocumentActionComponent = (props: DocumentActionProps) => {
+  const client = useClient()
   return {
     label: 'Duplicate',
     onHandle: () => {
       const { draft, published } = props
       const source = draft || published
       if (source) {
-        props.client.create({
+        client.create({
           ...source,
           _id: undefined, // let Sanity generate a new ID
           slug: undefined, // force user to add new slug
