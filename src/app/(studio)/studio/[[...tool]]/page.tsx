f03561ffc2b674d14884441805ef1f78f6cd6af6
olamaan <75326761+olamaan@users.noreply.github.com>
2025-09-19 07:52:15 -0400

    stuff

--- src/app/(studio)/studio/[[...tool]]/page.tsx
+++ src/app/(studio)/studio/[[...tool]]/page.tsx
@@ -11,7 +11,9 @@
 
 // src/app/(studio)/studio/[[...tool]]/page.tsx
 import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config' // ✅ relative to this file
+
+import config from '@/sanity.config'
+
 
 export const dynamic = 'force-static'
 export { metadata, viewport } from 'next-sanity/studio'
